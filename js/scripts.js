c1f3a8ea7900ccb59fc7380a033b27332dbcfef3
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2023-04-27 00:37:08 -0700

    Refactor createCard function

--- js/scripts.js
+++ js/scripts.js
@@ -18,37 +18,34 @@ function addCardToGallery(card) {
   gallery.insertAdjacentElement('beforeend', card);
 }
 
-function createCard(employee, data) {
-  const cardDiv = document.createElement('DIV');
-  const cardImageDiv = document.createElement('DIV');
-  const img = document.createElement('IMG');
-  const cardInfoDiv = document.createElement('DIV');
-  const nameH3 = document.createElement('H3');
-  const emailP = document.createElement('P');
-  const locationP = document.createElement('P');
+function createCard(employee) {
+  const card = document.createElement('DIV');
+  const cardImgContainer = document.createElement('DIV');
+  const cardImg = document.createElement('IMG');
+  const cardInfoContainer = document.createElement('DIV');
+  const name = document.createElement('H3');
+  const email = document.createElement('P');
+  const location = document.createElement('P');
 
-  cardDiv.className = 'card';
-  cardImageDiv.className = 'card-img-container';
-  img.className = 'card-img';
-  img.src = employee.picture.large;
-  img.alt = `${employee.name.first} ${employee.name.last}`;
-  cardInfoDiv.className = 'card-info-container';
-  nameH3.id = 'name';
-  nameH3.className = 'card-name cap';
-  nameH3.textContent = `${employee.name.first} ${employee.name.last}`;
-  emailP.className = 'card-text';
-  emailP.textContent = employee.email;
-  locationP.className = 'card-text cap';
-  locationP.textContent = `${employee.location.city}, ${employee.location.state}`;
+  card.className = 'card';
+  cardImgContainer.className = 'card-img-container';
+  cardImg.className = 'card-img';
+  cardImg.src = employee.picture.large;
+  cardImg.alt = `${employee.name.first} ${employee.name.last}`;
+  cardInfoContainer.className = 'card-info-container';
+  name.id = 'name';
+  name.className = 'card-name cap';
+  name.textContent = `${employee.name.first} ${employee.name.last}`;
+  email.className = 'card-text';
+  email.textContent = employee.email;
+  location.className = 'card-text cap';
+  location.textContent = `${employee.location.city}, ${employee.location.state}`;
 
-  cardDiv.insertAdjacentElement('afterbegin', cardImageDiv);
-  cardDiv.insertAdjacentElement('beforeend', cardInfoDiv);
-  cardImageDiv.insertAdjacentElement('afterbegin', img);
-  cardInfoDiv.insertAdjacentElement('afterbegin', nameH3);
-  cardInfoDiv.insertAdjacentElement('beforeend', emailP);
-  cardInfoDiv.insertAdjacentElement('beforeend', locationP);
+  card.append(cardImgContainer, cardInfoContainer);
+  cardImgContainer.append(cardImg);
+  cardInfoContainer.append(name, email, location);
 
-  return cardDiv;
+  return card;
 }
 
 function displayModal(employee, data) {
